7bdb8c5348cda2bf906c0ff866c42725ab2b2950
Andreu Sala <andreus.sala@gmail.com>
2016-05-06 20:41:51 +0200

    Codi no necessari a la classe Request

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -22,12 +22,8 @@ class Request{
 			$request = explode("/", $request);
 			$request = array_filter($request);
 			
-			if($request[0] == "index.php"){
-				$this->controller = "worker";
-			}else{
-				$this->controller = strtolower(array_shift($request));
-			}
-
+						
+			$this->controller = strtolower(array_shift($request));
 			$this->method = strtolower(array_shift($request));
 			if(!$this->method){
 				$this->method = "index";
