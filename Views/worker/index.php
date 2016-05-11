0afdaafd67d06c4608bec2e9a26dabaedf9761d7
krotus <andreus.sala@gmail.com>
2016-05-11 10:31:45 +0200

    Solucionat alguns bugs del css i missatge de alerta

--- Views/worker/index.php
+++ Views/worker/index.php
@@ -14,8 +14,8 @@ if (isset($data)) {
     <div class="col-md-1 hidden-xs">
     </div>
     <div class="panel panel-primary col-md-10 col-xs-12">
-        <div class="panel-heading">
-            <h3 data-toggle="collapse" data-parent="#accordion" href="#collapse1">Ordenes pendientes</h3>
+        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+            <h3>Ordenes pendientes</h3>
         </div>
         <div id="collapse1" class="panel-collapse collapse in">
             <div class="panel-body"> <table id="pending-ord" class="orders-table table table-striped table-bordered"></table></div>
@@ -29,8 +29,8 @@ if (isset($data)) {
     <div class="col-md-1 hidden-xs">
     </div>
     <div class="panel panel-primary col-md-10 col-xs-12">
-        <div class="panel-heading">
-            <h3 data-toggle="collapse" data-parent="#accordion" href="#collapse2">Ordenes inicializadas</h3>
+        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+            <h3>Ordenes inicializadas</h3>
         </div>
         <div id="collapse2" class="panel-collapse collapse">
             <div class="panel-body"> <table id="init-ord" class="orders-table table table-striped table-bordered"></table></div>
@@ -44,8 +44,8 @@ if (isset($data)) {
     <div class="col-md-1 hidden-xs">
     </div>
     <div class="panel panel-primary col-md-10 col-xs-12">
-        <div class="panel-heading">
-            <h3 data-toggle="collapse" data-parent="#accordion" href="#collapse3">Ordenes completadas</h3>
+        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+            <h3>Ordenes completadas</h3>
         </div>
         <div id="collapse3" class="panel-collapse collapse">
             <div class="panel-body"> <table id="completed-ord" class="orders-table table table-striped table-bordered"></table></div>
@@ -58,8 +58,8 @@ if (isset($data)) {
     <div class="col-md-1 hidden-xs">
     </div>
     <div class="panel panel-primary col-md-10 col-xs-12">
-        <div class="panel-heading">
-            <h3 data-toggle="collapse" data-parent="#accordion" href="#collapse4">Ordenes no completadas</h3>
+        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+            <h3>Ordenes no completadas</h3>
         </div>
         <div id="collapse4" class="panel-collapse collapse">
             <div class="panel-body"> <table id="uncompleted-ord" class="orders-table table table-striped table-bordered"></table></div>
@@ -73,8 +73,8 @@ if (isset($data)) {
     <div class="col-md-1 hidden-xs">
     </div>
     <div class="panel panel-primary col-md-10 col-xs-12">
-        <div class="panel-heading">
-            <h3 data-toggle="collapse" data-parent="#accordion" href="#collapse5">Ordenes canceladas</h3>
+        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+            <h3>Ordenes canceladas</h3>
         </div>
         <div id="collapse5" class="panel-collapse collapse ">
             <div class="panel-body"> <table id="cancelled-ord" class="orders-table table table-striped table-bordered"></table></div>
