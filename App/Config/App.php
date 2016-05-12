bdc0cb1b7f3688adb26e986227514369344c9ac2
root <joanpont19@gmail.com>
2016-05-12 18:30:02 +0200

    merge solve

--- App/Config/App.php
+++ App/Config/App.php
@@ -10,7 +10,7 @@ define("FIRST_PAGE_ADMIN", "dashboard");
 
 
 //WEBSERVICE 
-define("WEBSERVICE", "http://testservice.xyz/v1/");
-//define("WEBSERVICE", "http://localhost/api.arduino.com/v1/");
+//define("WEBSERVICE", "http://testservice.xyz/v1/");
+define("WEBSERVICE", "http://localhost/api.arduino.com/v1/");
 
 ?>
--- Controllers/Login/LoginController.php
+++ Controllers/Login/LoginController.php
@@ -16,7 +16,7 @@ class LoginController{
 			$user = new Worker();
     		Session::set("user",$user);
             $user->setId(1);
-            $user->get();
+            $user->getAll();
             //var_dump();
             //exit;
     		if(Session::get("user") instanceof Worker){
