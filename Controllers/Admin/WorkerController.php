11aa09cc0cefc096e3fc1c08b4a826e695c77963
krotus <andreus.sala@gmail.com>
2016-05-15 12:29:24 +0200

    exemple php validator worker create

--- Controllers/Admin/WorkerController.php
+++ Controllers/Admin/WorkerController.php
@@ -7,6 +7,7 @@ use Models\Business\Worker as Worker;
 use Models\Business\Worker as Admin;
 use App\Core\Session as Session;
 use App\Core\View as View;
+use Respect\Validation\Validator as v;
 
 class WorkerController extends Controller {
 
@@ -25,7 +26,73 @@ class WorkerController extends Controller {
     }
 
     public function create() {
-        View::to("admin.worker.create");
+        if(!$_POST){
+            View::to("admin.worker.create");
+        }else{
+            $error = "";
+            $username = $_POST["worker_username"];
+            if(v::notEmpty()->validate($username)){
+                $password = $_POST["worker_password"];
+                if(v::notEmpty()->validate($password)){
+                    $rePassword = $_POST["worker_re_password"];
+                    if(v::equals($password)->validate($rePassword)){
+                        $nif = $_POST["worker_NIF"];
+                        if(v::notEmpty()->validate($nif)){
+                            if(v::IdentityCardEs()->validate($nif)){
+                                $name = $_POST["worker_name"];
+                                if(v::notEmpty()->validate($name)){
+                                    $surname = $_POST["worker_surname"];
+                                    if(v::notEmpty()->validate($surname)){
+                                        $telephone = $_POST["worker_telephone"];
+                                        if(v::length(9,null)->validate($telephone)){
+                                            $category = $_POST["worker_category"];
+                                            if(v::notEmpty()->validate($category)){
+                                                $teamId = $_POST["worker_team"];
+                                                if(v::notEmpty()->validate($teamId)){
+                                                    $isAdmin = $_POST["worker_is_admin"];
+                                                    if(v::notEmpty()->validate($isAdmin)){
+
+                                                    }else{
+                                                        $error = "select is/not admin";
+                                                    }
+                                                }else{
+                                                    $error = "empty team";
+                                                }
+                                            }else{
+                                                $error = "empty category";
+                                            }
+                                        }else{
+                                            $error = "incorrect telephone";
+                                        }
+                                    }else{
+                                        $error = "empty surname";
+                                    }
+                                }else{
+                                    $error = "empty name";
+                                }
+                            }else{
+                                $error = "nif incorrect";
+                            }
+                        }else{
+                            $error = "empty nif";
+                        }
+                    }else{
+                        $error = "passwords not match";
+                    }
+                }else{
+                    $error = "empty password";
+                }
+            }else{
+                $error = "empty username";
+            }
+
+            if(v::notEmpty()->validate($error)){
+                View::redirect("admin.worker.create", compact('error'));
+            }else{
+                echo "create worker";
+            }
+
+        }
     }
 
 }
--- Views/admin/worker/create.php
+++ Views/admin/worker/create.php
@@ -1,4 +1,51 @@
-<?php
-
-echo 'create';
+<?php 
+if(isset($data)){
+	if (array_key_exists("error", $data)) {
+		echo '<div class="alert alert-danger" role="alert">' . $data["error"] . '</div>';
+	}	
+}
 ?>
+<form role="form" action="" method="POST">
+	<div class="form-group">
+		<label for="worker_username">Username:</label>
+		<input type="text" class="form-control" name="worker_username" id="worker_username">
+	</div>
+	<div class="form-group">
+		<label for="worker_password">Password:</label>
+		<input type="password" class="form-control" name="worker_password" id="worker_password">
+	</div>
+	<div class="form-group">
+		<label for="worker_re_password">Re-Password:</label>
+		<input type="password" class="form-control" name="worker_re_password" id="worker_re_password">
+	</div>
+	<div class="form-group">
+		<label for="worker_NIF">NIF:</label>
+		<input type="text" class="form-control" name="worker_NIF" id="worker_NIF">
+	</div>
+	<div class="form-group">
+		<label for="worker_name">Name:</label>
+		<input type="text" class="form-control" name="worker_name" id="worker_name">
+	</div>
+	<div class="form-group">
+		<label for="worker_surname">Surname:</label>
+		<input type="text" class="form-control" name="worker_surname" id="worker_surname">
+	</div>
+	<div class="form-group">
+		<label for="worker_telephone">Telephone:</label>
+		<input type="text" class="form-control" name="worker_telephone" id="worker_telephone">
+	</div>
+	<div class="form-group">
+		<label for="worker_category">Category:</label>
+		<input type="text" class="form-control" name="worker_category" id="worker_category">
+	</div>
+	<div class="form-group">
+		<label for="worker_team">Team:</label>
+		<input type="text" class="form-control" name="worker_team" id="worker_team">
+	</div>
+	<div class="form-group">
+		<label for="worker_is_admin">Is Admin:</label>
+		<label class="radio-inline"><input type="radio" name="worker_is_admin">Yes</label>
+		<label class="radio-inline"><input type="radio" name="worker_is_admin">No</label>
+	</div>
+	<button type="submit" class="btn btn-default">Submit</button>
+</form>
