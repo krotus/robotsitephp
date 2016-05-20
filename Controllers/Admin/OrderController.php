93e9125144c49694efac53e4054ff2d2550424dc
carlosenricproyecto <carlos.enric.proyecto@gmail.com>
2016-05-20 20:38:10 +0200

    vistas backend done

--- Controllers/Admin/OrderController.php
+++ Controllers/Admin/OrderController.php
@@ -22,7 +22,7 @@ class OrderController extends Controller {
     }
 
     public function edit($id) {
-        if(!$_POST){
+        if (!$_POST) {
             $order = new Order($id);
             $order = $order->get();
             $robot = new Robot();
@@ -31,106 +31,111 @@ class OrderController extends Controller {
             $processes = $process->getAll();
             $statusOrder = new StatusOrder();
             $statusOrders = $statusOrder->getAll();
-            View::to("admin.order.edit", compact('order','robots','processes','statusOrders'));
+            View::to("admin.order.edit", compact('order', 'robots', 'processes', 'statusOrders'));
         } else {
             $validator = new Gump();
             $inputs = array(
-                'order_code'              =>  $_POST["order_code"],
-                'order_description'       =>  $_POST["order_description"],
-                'order_priority'          =>  $_POST["order_priority"],
-                'order_date'              =>  $_POST["order_date"],
-                'order_quantity'          =>  $_POST["order_quantity"],
-                'order_status'            =>  $_POST["order_status"],
-                'order_robot'             =>  $_POST["order_robot"],
-                'order_process'           =>  $_POST["order_process"]
+                'order_code' => $_POST["order_code"],
+                'order_description' => $_POST["order_description"],
+                'order_priority' => $_POST["order_priority"],
+                'order_date' => $_POST["order_date"],
+                'order_quantity' => $_POST["order_quantity"],
+                'order_status' => $_POST["order_status"],
+                'order_robot' => $_POST["order_robot"],
+                'order_process' => $_POST["order_process"]
             );
             $rules = array(
-                'order_code'              =>  'required|numeric|min_len,2',
-                'order_description'       =>  'required|max_len,50|min_len,3',
-                'order_priority'          =>  'required',
-                'order_date'              =>  'required',
-                'order_quantity'          =>  'required|numeric|min_len,1',
-                'order_status'            =>  'required',
-                'order_robot'             =>  'required',
-                'order_process'           =>  'required'
+                'order_code' => 'required|numeric|min_len,2',
+                'order_description' => 'required|max_len,50|min_len,3',
+                'order_priority' => 'required',
+                'order_date' => 'required',
+                'order_quantity' => 'required|numeric|min_len,1',
+                'order_status' => 'required',
+                'order_robot' => 'required',
+                'order_process' => 'required'
             );
             $validated = $validator->validate($inputs, $rules);
-            
-            if($validated === TRUE){
+
+            if ($validated === TRUE) {
                 $admin = unserialize(Session::get("user"));
                 $admin->updateOrder(new Order(
-                    $id,
-                    $_POST["order_code"],
-                    $_POST["order_description"],
-                    $_POST["order_priority"],
-                    $_POST["order_date"],
-                    $_POST["order_quantity"],
-                    $_POST["order_status"],
-                    $_POST["order_robot"],
-                    $_POST["order_process"]
-                    ));
+                        $id, $_POST["order_code"], $_POST["order_description"], $_POST["order_priority"], $_POST["order_date"], $_POST["order_quantity"], $_POST["order_status"], $_POST["order_robot"], $_POST["order_process"]
+                ));
                 $msg = "s'ha editat satisfactoriament.";
                 View::redirect("admin.order", compact("msg"));
-            }else{
+            } else {
                 $error = $validator->get_readable_errors(false);
-                View::redirect("admin.order.edit.".$id, compact('error'));
+                View::redirect("admin.order.edit." . $id, compact('error'));
             }
         }
     }
 
     public function delete($id) {
-        View::to("admin.order.delete");
+        $order = new Order($id);
+        $order->delete();
     }
 
     public function create() {
-        if(!$_POST){
+        if (!$_POST) {
             $robot = new Robot();
             $robots = $robot->getAll();
             $process = new Process();
             $processes = $process->getAll();
-            View::to("admin.order.create", compact('robots','processes'));
-        }else{
+            View::to("admin.order.create", compact('robots', 'processes'));
+        } else {
             $validator = new Gump();
             $inputs = array(
-                'order_code'              =>  $_POST["order_code"],
-                'order_description'       =>  $_POST["order_description"],
-                'order_priority'          =>  $_POST["order_priority"],
-                'order_date'              =>  $_POST["order_date"],
-                'order_quantity'          =>  $_POST["order_quantity"],
-                'order_robot'             =>  $_POST["order_robot"],
-                'order_process'           =>  $_POST["order_process"]
+                'order_code' => $_POST["order_code"],
+                'order_description' => $_POST["order_description"],
+                'order_priority' => $_POST["order_priority"],
+                'order_date' => $_POST["order_date"],
+                'order_quantity' => $_POST["order_quantity"],
+                'order_robot' => $_POST["order_robot"],
+                'order_process' => $_POST["order_process"]
             );
             $rules = array(
-                'order_code'              =>  'required|numeric|min_len,3',
-                'order_description'       =>  'required|max_len,50|min_len,3',
-                'order_priority'          =>  'required',
-                'order_date'              =>  'required',
-                'order_quantity'          =>  'required|numeric|min_len,1',
-                'order_robot'             =>  'required',
-                'order_process'           =>  'required'
+                'order_code' => 'required|numeric|min_len,3',
+                'order_description' => 'required|max_len,50|min_len,3',
+                'order_priority' => 'required',
+                'order_date' => 'required',
+                'order_quantity' => 'required|numeric|min_len,1',
+                'order_robot' => 'required',
+                'order_process' => 'required'
             );
             $validated = $validator->validate($inputs, $rules);
 
-            if($validated === TRUE){
+            if ($validated === TRUE) {
                 $admin = unserialize(Session::get("user"));
                 $admin->createOrder(new Order(
-                    null,
-                    $_POST["order_code"],
-                    $_POST["order_description"],
-                    $_POST["order_priority"],
-                    $_POST["order_date"],
-                    $_POST["order_quantity"],
-                    1,//statusOrder "pending"
-                    $_POST["order_robot"],
-                    $_POST["order_process"]
-                    ));
+                        null, $_POST["order_code"], $_POST["order_description"], $_POST["order_priority"], $_POST["order_date"], $_POST["order_quantity"], 1, //statusOrder "pending"
+                        $_POST["order_robot"], $_POST["order_process"]
+                ));
                 $msg = "s'ha creat satisfactoriament.";
                 View::redirect("admin.order", compact("msg"));
-            }else{
+            } else {
                 $error = $validator->get_readable_errors(false);
                 View::redirect("admin.order.create", compact('error'));
             }
         }
+    }
+
+    function getOrdersByAjax() {
+        ob_end_clean();
+        $order = new Order();
+        $orders = $order->getAllOrdersAdmin();
+//        Debug::log($orders);
+//        exit();
+        $arrayToSend = array();
+        for ($i = 0; $i < count($orders); $i++) {
+            $auxArray = array();
+            foreach ($orders[$i] as $nOrder) {
+                array_push($auxArray, $nOrder);
+            }
+            array_push($auxArray, "<a href='" . URL . "admin/order/edit/" . $orders[$i]['order_id'] . "'><button class='btn btn-primary'><span class='glyphicon glyphicon-pencil'></span></button></a><button class='btn btn-danger' onclick='deleteOrder(" . $orders[$i]['order_id'] . ", \"" . URL . "\");'><span class='glyphicon glyphicon-remove'></span></button>");
+            array_shift($auxArray);
+            array_push($arrayToSend, $auxArray);
+        }
+        echo json_encode($arrayToSend);
     }
 
 }
--- Views/admin/order/index.php
+++ Views/admin/order/index.php
@@ -2,12 +2,13 @@
 <div class="row">
     <div class="col-xs-12">
 		<h2>Ordenes</h2>
+                <a href="<?php echo URL."admin/order/create" ?>"><button id="create-button" class="btn btn-primary">Añadir Orden</button></a>
         <table id="ordersList" class="table table-bordered table-condensed">
         </table>
     </div>
 </div>
 <script type="text/javascript">
     $(document).ready(function(){
-        orderListShow();
+        orderListShow('<?php echo URL; ?>');
     });
 </script>
