a6a2fd696a70e5d61f7c29e8099b43a3a54bb17a
carlosenricproyecto <carlos.enric.proyecto@gmail.com>
2016-05-20 16:45:15 +0200

    admin team done

--- Controllers/Admin/TeamController.php
+++ Controllers/Admin/TeamController.php
@@ -21,72 +21,88 @@ class TeamController extends Controller {
     }
 
     public function edit($id) {
-        if(!$_POST){
-        $team = new Team($id);
-        $team = $team->get();
-        View::to("admin.team.edit", compact('team'));
-        }else{
+        if (!$_POST) {
+            $team = new Team($id);
+            $team = $team->get();
+            View::to("admin.team.edit", compact('team'));
+        } else {
             $validator = new Gump();
             $inputs = array(
-                'team_code'       =>  $_POST["team_code"],
-                'team_name'       =>  $_POST["team_name"]
+                'team_code' => $_POST["team_code"],
+                'team_name' => $_POST["team_name"]
             );
             $rules = array(
-                'team_code'       =>  'required|numeric|min_len,3',
-                'team_name'       =>  'required|max_len,50|min_len,3',
+                'team_code' => 'required|numeric|min_len,3',
+                'team_name' => 'required|max_len,50|min_len,3',
             );
             $validated = $validator->validate($inputs, $rules);
 
-            if($validated === TRUE){
+            if ($validated === TRUE) {
                 $admin = unserialize(Session::get("user"));
                 $admin->updateTeam(new Team(
-                    $id,
-                    $_POST["team_code"],
-                    $_POST["team_name"]
-                    ));
-                
+                        $id, $_POST["team_code"], $_POST["team_name"]
+                ));
+
                 $msg = "S'ha editat satisfactoriament.";
                 View::redirect("admin.team", compact("msg"));
-            }else{
+            } else {
                 $error = $validator->get_readable_errors(false);
-                View::redirect("admin.team.edit.". $id, compact('error'));
+                View::redirect("admin.team.edit." . $id, compact('error'));
             }
         }
     }
 
     public function delete($id) {
-        View::to("admin.team.delete");
+        $team = new Team($id);
+        $team->delete();
     }
 
     public function create() {
-        if(!$_POST){
+        if (!$_POST) {
             View::to("admin.team.create");
-        }else{
+        } else {
             $validator = new Gump();
             $inputs = array(
-                'team_code'       =>  $_POST["team_code"],
-                'team_name'       =>  $_POST["team_name"]
+                'team_code' => $_POST["team_code"],
+                'team_name' => $_POST["team_name"]
             );
             $rules = array(
-                'team_code'       =>  'required|numeric|min_len,3',
-                'team_name'       =>  'required|max_len,50|min_len,3',
+                'team_code' => 'required|numeric|min_len,3',
+                'team_name' => 'required|max_len,50|min_len,3',
             );
             $validated = $validator->validate($inputs, $rules);
 
-            if($validated === TRUE){
+            if ($validated === TRUE) {
                 $admin = unserialize(Session::get("user"));
                 $admin->createTeam(new Team(
-                    null,
-                    $_POST["team_code"],
-                    $_POST["team_name"]
-                    ));
+                        null, $_POST["team_code"], $_POST["team_name"]
+                ));
                 $msg = "S'ha creat satisfactoriament.";
                 View::redirect("admin.team", compact("msg"));
-            }else{
+            } else {
                 $error = $validator->get_readable_errors(false);
                 View::redirect("admin.team.create", compact('error'));
             }
         }
+    }
+
+    function getTeamsByAjax() {
+        ob_end_clean();
+        $team = new Team();
+        $teams = $team->getAllTeamsAdmin();
+        //Debug::log($teams);
+        $arrayToSend = array();
+        for ($i = 0; $i < count($teams); $i++) {
+            $auxArray = array();
+            foreach ($teams[$i] as $nTeam) {
+                array_push($auxArray, $nTeam);
+            }
+            array_push($auxArray, "<a href='" . URL . "admin/team/edit/" . $teams[$i]['id'] . "'><button class='btn btn-primary'><span class='glyphicon glyphicon-pencil'></span></button></a><button class='btn btn-danger' onclick='deleteTeam(" . $teams[$i]['id'] . ", \"" . URL . "\");'><span class='glyphicon glyphicon-remove'></span></button>");
+            array_shift($auxArray);
+            array_push($arrayToSend, $auxArray);
+        }
+        //Debug::log($arrayToSend);
+        echo json_encode($arrayToSend);
     }
 
 }
