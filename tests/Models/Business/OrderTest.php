9dbd717b50aec2e3402ccb360d6dbd9f4251c5db
markk15 <mmaark15@gmail.com>
2016-05-27 13:17:27 +0200

    phpunit tests done

--- tests/Models/Business/OrderTest.php
+++ tests/Models/Business/OrderTest.php
@@ -11,66 +11,72 @@
  *
  * @author Marc
  */
+use Models\Business\Order as Order;
+use Models\Business\StatusOrder as StatusOrder;
+use Models\Business\Robot as Robot;
+use Models\Business\Process as Process;
+
 class OrderTest extends PHPUnit_Framework_TestCase{
     protected static $order;
 
 	public static function setUpBeforeClass(){
-        self::$order = new Models\Business\Order();
+        self::$order = new Order;
     }
 
     public static function tearDownAfterClass(){
         self::$order = NULL;
     }
-    
-	public function testCode($code){
-		self::$order->setCode($code);
+	public function testCode(){
+		self::$order->setCode(100);
 		$this->assertGreaterThan(99, self::$order->getCode());
-		$this->assertType("int", self::$order->getCode());
+		$this->assertInternalType("int", self::$order->getCode());
 	}
         
-        public function testDescription($description){
-            self::$order->setDescription($description);
-            $this->assertGreaterThan(3, str_len(self::$order->getDescription()));
-            $this->assertLessThan(50, str_len(self::$order->getDescription()));
+        public function testDescription(){
+            self::$order->setDescription('prova');
+            $this->assertGreaterThan(3, strlen(self::$order->getDescription()));
+            $this->assertLessThan(50, strlen(self::$order->getDescription()));
         }
 
-        public function testPriority($priority){
-            self::$order->setPriority($priority);
+        public function testPriority(){
+            self::$order->setPriority(5);
             $this->assertGreaterThan(0, self::$order->getPriority());
             $this->assertLessThan(10, self::$order->getPriority());
-            $this->assertType("int", self::$order->getPriority());
+            $this->assertInternalType("int", self::$order->getPriority());
 	}
-        
-        public function testDate($date){
-            self::$order->setDate($date);
-        }
-        
-        public function testQuantity($quantity){
-		self::$order->setQuantity($quantity);
+//        
+//        public function testDate($date){
+//            self::$order->setDate($date);
+//        }
+//        
+        public function testQuantity(){
+		self::$order->setQuantity(99);
 		$this->assertGreaterThan(0, self::$order->getQuantity());
-		$this->assertType("int", self::$order->getQuantity());
+		$this->assertInternalType("int", self::$order->getQuantity());
 	}
         
-        public function testStatusOrder($idstatusOrder){
-		self::$order->getStatusOrder()->setId($idstatusOrder);
-		$this->assertGreaterThan(0, self::$order->getStatusOrder()->getId());
-		$this->assertType("int", self::$order->getStatusOrder()->getId());
+        public function testStatusOrder(){
+            $statusOrder = new StatusOrder;
+            self::$order->setStatusOrder($statusOrder);
+            self::$order->getStatusOrder()->setId(5);
+            $this->assertGreaterThan(0, self::$order->getStatusOrder()->getId());
+            $this->assertInternalType("int", self::$order->getStatusOrder()->getId());
 	}
         
-        public function testRobot($idrobot){
-		self::$order->getRobot()->setId($idrobot);
-		$this->assertGreaterThan(0, self::$order->getRobot()->getId());
-		$this->assertType("int", self::$order->getRobot()->getId());
+        public function testRobot(){
+            $robot = new Robot;
+            self::$order->setRobot($robot);
+            self::$order->getRobot()->setId(1);
+            $this->assertGreaterThan(0, self::$order->getRobot()->getId());
+            $this->assertInternalType("int", self::$order->getRobot()->getId());
 	}
         
-        public function testProcess($idprocess){
-		self::$order->getProcess()->setId($idprocess);
-		$this->assertGreaterThan(0, self::$order->getProcess()->getId());
-		$this->assertType("int", self::$order->getProcess()->getId());
+        public function testProcess(){
+            $process = new Process;
+            self::$order->setProcess($process);
+            self::$order->getProcess()->setId(3);
+            $this->assertGreaterThan(0, self::$order->getProcess()->getId());
+            $this->assertInternalType("int", self::$order->getProcess()->getId());
 	}
         
-//	public function testSetCode(){
-//		self::$order->setCode(100);
-//		$this->assertEquals(100, self::$order->getCode());
-//	}
 }
